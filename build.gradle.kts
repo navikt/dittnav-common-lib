fc9dfb9949d9eed1114ded8cb5ff5d88ed0c955a
Christopher Santana <christopher.santana@nav.no>
2020-08-07 11:09:03 +0200

    Split workflow, such that version is managed from github.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     kotlin("plugin.allopen") version kotlinVersion
 }
 
-val libraryVersion = "0.0.1"
+val libraryVersion = properties["version"] ?: "latest-local"
 
 subprojects {
     group = "no.nav"
