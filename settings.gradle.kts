ad92ae4745476b65a55658a151dae4f0b2e465fd
Christopher Santana <christopher.santana@nav.no>
2021-01-28 13:07:29 +0100

    Fjernet dittnav-common-test fra settings.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,5 @@ rootProject.name = "dittnav-common-lib"
 include("dittnav-common-logging")
 include("dittnav-common-influx")
 include("dittnav-common-security-authenticated-user")
-include("dittnav-common-test")
 include("dittnav-common-utils")
 include("dittnav-common-evicting-cache")
